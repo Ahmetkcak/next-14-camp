5a2ac155f00c72d1ed89d93244590dcd9261423d
Ahmetkcak <ahmet.aliberk07@gmail.com>
2024-06-19 21:30:45 +0300

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,15 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+  title:"Blog"
+}
+
+// export const metadataProp : Metadata = {
+//   title:{
+//     absolute :""
+//   }
+// }
+
 export default function Blog() {
     return <h1>Blog Page</h1>
   }
