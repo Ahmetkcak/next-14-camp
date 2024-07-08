eedaab4542e67bb9ca03f8a58ca1d68ab6bc44bb
Ahmetkcak <ahmet.aliberk07@gmail.com>
2024-07-08 11:54:56 +0300

    static rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
     console.log("About server Component");
   return (    
     <div>
-      <h1>About Page</h1>
+      <h1>About Page {new Date().toLocaleTimeString()}</h1>
     </div>
   )
 }
