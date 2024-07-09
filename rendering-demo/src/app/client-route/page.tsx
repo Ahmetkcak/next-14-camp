6d098ebcbe30afc53ad69eaca10506e3859040b9
Ahmetkcak <ahmet.aliberk07@gmail.com>
2024-07-09 21:08:10 +0300

    client-only Code

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,23 +1,19 @@
 'use client'
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFuntion } from "@/utils/client-utils";
 
 
 export default function ClientRoutePage() {
   const theme = useTheme();
-  const settings = {
-    dots: true
-  }
+  const result = clientSideFuntion();
   return (
     <>
       <h1 style={{
         color: theme.colors.primary
       }}
       >
-        Client Route
+        Client Route {result}
       </h1>
     </>
   );
---
+++ rendering-demo/src/utils/client-utils.ts
@@ -0,0 +1,9 @@
+import "client-only";
+
+export const clientSideFuntion = () => {
+    console.log(
+        `use window object
+        use localstorage`
+    );
+    return "client result";
+}
